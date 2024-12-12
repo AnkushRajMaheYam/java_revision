8afcc8f5f406eb8c5ef2d38c5c64bb3f74e258b3
Ankush Raj Mahe Yam <ankushrajmaheyam@gmaill.com>
2024-12-12 21:15:56 +0530

    page86 added

---
+++ oops/page86/Employ.java
@@ -0,0 +1,18 @@
+class Employ
+{
+	static String comp_name="ARMY Pvt Ltd";
+	String name;
+	int id;
+	double salary;
+	public static void work(){
+		System.out.println("EMploy is working");
+	}
+	public void read(){
+		System.out.println("EMploy is reading");
+	}
+	
+}
+
+
+
+
